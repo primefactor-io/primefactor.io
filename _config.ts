d86c0ebe490ace646babb16d8b44552052be79ec
Philipp Muens <philipp@muens.io>
2025-04-28 08:36:35 +0200

    Open external URLs in new tab

--- _config.ts
+++ _config.ts
@@ -1,5 +1,18 @@
 import lume from "lume/mod.ts";
+import { Data, Page } from "lume/core/file.ts";
 
-const site = lume({ src: "./src" });
+import modifyUrls from "lume/plugins/modify_urls.ts";
+
+const site = lume({ src: "./src" })
+  .use(modifyUrls({
+    extensions: [".html"],
+    fn: (url: string, page: Page<Data>) => {
+      if (url.startsWith("http")) {
+        const link = page.document?.querySelector(`a[href="${url}"]`);
+        if (link) link.setAttribute("target", "_blank");
+      }
+      return url;
+    },
+  }));
 
 export default site;
