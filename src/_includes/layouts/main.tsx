1686de6ab9250780b04da6a4fe91b526f060b9fd
Philipp Muens <philipp@muens.io>
2025-04-28 09:40:22 +0200

    Add canonical link element

--- src/_includes/layouts/main.tsx
+++ src/_includes/layouts/main.tsx
@@ -2,12 +2,15 @@ export const title = "Default Main Title";
 export const description = "Default Main Description";
 export const language = "en";
 
-export default function ({ title, language, children, comp }: Lume.Data) {
+export default function (
+  { title, language, url, domain, children, comp }: Lume.Data,
+) {
   return (
     <html lang={language}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href={String(new URL(url, domain))} />
         <link rel="stylesheet" type="text/css" href="/styles.css" />
         <title>{title}</title>
       </head>
