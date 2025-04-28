1c7d8695c694873e83546dab2fa56697c5c16782
Philipp Muens <philipp@muens.io>
2025-04-28 09:02:48 +0200

    Add favicon

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import jsx from "lume/plugins/jsx.ts";
 import nav from "lume/plugins/nav.ts";
 import robots from "lume/plugins/robots.ts";
 import postcss from "lume/plugins/postcss.ts";
+import favicon from "lume/plugins/favicon.ts";
 import modifyUrls from "lume/plugins/modify_urls.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 
@@ -12,6 +13,9 @@ const site = lume({ src: "./src" })
   .use(jsx())
   .use(nav())
   .use(robots())
+  .use(favicon({
+    input: "/public/favicon.png",
+  }))
   .use(tailwindcss())
   .use(postcss()) // Note: The PostCSS plugin needs to be added after the Tailwind CSS plugin.
   .use(modifyUrls({
