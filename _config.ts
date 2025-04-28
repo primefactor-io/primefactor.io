2ee3423197eff546e88a5bbbca9bfe845976df09
Philipp Muens <philipp@muens.io>
2025-04-28 08:54:06 +0200

    Use plugin to generate robots.txt

--- _config.ts
+++ _config.ts
@@ -3,11 +3,13 @@ import { Data, Page } from "lume/core/file.ts";
 
 import jsx from "lume/plugins/jsx.ts";
 import nav from "lume/plugins/nav.ts";
+import robots from "lume/plugins/robots.ts";
 import modifyUrls from "lume/plugins/modify_urls.ts";
 
 const site = lume({ src: "./src" })
   .use(jsx())
   .use(nav())
+  .use(robots())
   .use(modifyUrls({
     extensions: [".html"],
     fn: (url: string, page: Page<Data>) => {
