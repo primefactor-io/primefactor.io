590c9fe924c6678b4289f669abdfb5e2b5ab0e21
Philipp Muens <philipp@muens.io>
2025-04-29 08:52:39 +0200

    Update content of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
+// deno-lint-ignore-file no-explicit-any
+
 export const title = null;
-export const description = "Index Page Description";
 export const url = "/";
 export const navigation = "Home";
 
-export default function () {
+export default function ({ name, description, socials }: Lume.Data) {
+  const github = socials.find((item: any) => item.name === "GitHub");
+
   return (
-    <>
-      <p>Index Page Body.</p>
-      <a href="/about/">Internal Link</a>
-      <a href="https://example.com">External Link</a>
-    </>
+    <div>
+      <hgroup>
+        <h1>{name}</h1>
+        <p role="doc-subtitle">{description}</p>
+      </hgroup>
+      <div>
+        <a href={github.url}>Explore Projects</a>
+      </div>
+    </div>
   );
 }
